a48eb17abf7ff05e071f08b2999550f0edb7e3e8
Chitransh-j <chitranshjawere3@gmail.com>
2024-06-27 22:22:58 +0530

    added db and dockerized

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type:'mysql',
+    host:'mysql_db',
+    port:3307,
+    database: 'nestjsdocker',
+    entities: [],
+    username: 'testuser',
+    password:'testuser123',
+    synchronize:true
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
